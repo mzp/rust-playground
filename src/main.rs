d5c07e570d27aedcb20f785c2dbc8f1af319ec2f
mzp <mzpppp@gmail.com>
2017-04-19 22:32:52 +0900

    :sparkles: implement cat command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::io::prelude::*;
+use std::fs::File;
+use std::env;
+
+fn read_all(path : String, mut buffer : &mut String) -> std::io::Result<usize> {
+    return File::open(path)
+        .and_then(|mut f| f.read_to_string(&mut buffer));
+}
+
 fn main() {
-    println!("Hello, world!");
+    for path in env::args().skip(1) {
+        let mut buffer = String::new();
+
+        match read_all(path, &mut buffer) {
+            Ok(_) => { print!("{}", buffer) }
+            Err(why) => { println!("ERR: {}", why.to_string()) }
+        }
+    }
 }
