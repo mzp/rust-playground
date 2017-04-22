4b3a8448fc4c7851e4c62f577309d8006625fa46
mzp <mzpppp@gmail.com>
2017-04-22 13:34:43 +0900

    :lipstick: refine cat

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,17 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::env;
 
-fn read_all(path : String, mut buffer : &mut String) -> std::io::Result<usize> {
+fn read_all(path : String) -> std::io::Result<String> {
+    let mut buffer = String::new();
     return File::open(path)
-        .and_then(|mut f| f.read_to_string(&mut buffer));
+        .and_then(|mut f| f.read_to_string(&mut buffer))
+        .map(|_| buffer);
 }
 
 fn main() {
     for path in env::args().skip(1) {
-        let mut buffer = String::new();
-
-        match read_all(path, &mut buffer) {
-            Ok(_) => { print!("{}", buffer) }
+        match read_all(path) {
+            Ok(content) => { print!("{}", content) }
             Err(why) => { println!("ERR: {}", why.to_string()) }
         }
     }
